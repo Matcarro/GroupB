ffac641a51fef8ca43004f854c7215afd400e7b4
Oscar De Filpo <oscar.defilpo@dechit.it>
2022-04-14 13:17:57 +0200

    Refactoring algoritmo checkString

--- src/main/java/com/course/model/checkstring/algorithms/Equal.java
+++ src/main/java/com/course/model/checkstring/algorithms/Equal.java
@@ -2,7 +2,7 @@ package com.course.model.checkstring.algorithms;
 
 import com.course.model.checkstring.CheckStringBase;
 
-public class Start extends CheckStringBase {
+public class Equal extends CheckStringBase {
 
 	@Override
 	protected int internalCheck(String input, String standard) {
